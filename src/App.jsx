63d152cae4f6f4cf60a1c955ea738d31e512ce80
Anisha2704 <anishamalore45@gmail.com>
2026-03-20 07:48:47 +0530

    created folder structure and global css

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,8 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import SearchSection from "./components/SearchSection";
-import EventsGrid from "./components/EventsGrid";
-import Stats from "./components/Stats";
-import FeaturedColleges from "./components/FeaturedColleges";
-import FeaturedEvents from "./components/FeaturedEvents";
-import HowItWorks from "./components/HowItWorks";
-import CTA from "./components/CTA";
-import Footer from "./components/Footer";
+import AppRoutes from "./routes/AppRoutes";
+
 
 function App() {
-  return (
-    <div className="bg-[#0B0F19] text-white min-h-screen">
-
-      <Navbar />
-
-      <Hero />
-
-      <SearchSection />
-
-      <FeaturedEvents />
-
-      <EventsGrid />
-
-      <Stats />
-
-      <FeaturedColleges />
-
-      <HowItWorks />
-
-      <CTA/>
-
-      <Footer/>
-
-      
-
-    </div>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes } from "react-router-dom";
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+       
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default AppRoutes;
